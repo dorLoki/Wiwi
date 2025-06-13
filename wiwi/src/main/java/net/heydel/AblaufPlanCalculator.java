ec84b3ce38d245cbd7cf902ccc59c1025070b34c
dorLoki <luke.heydel@outlook.de>
2025-06-13 22:07:35 +0200

    better ablaufplan calc

--- wiwi/src/main/java/net/heydel/AblaufPlanCalculator.java
+++ wiwi/src/main/java/net/heydel/AblaufPlanCalculator.java
@@ -23,7 +23,13 @@ public class AblaufPlanCalculator {
 
     public List<CsvColumn> calc() {
         calcAblauf();
-        return result;
+        return result /*
+                       * .stream()
+                       * .sorted(Comparator
+                       * .comparingInt(CsvColumn::getTeilId)
+                       * .thenComparing(CsvColumn::getBeginnDerFertigung))
+                       * .collect(Collectors.toList())
+                       */;
     }
 
     private void calcAblauf() {
@@ -45,6 +51,7 @@ public class AblaufPlanCalculator {
                         .orElseThrow(
                                 () -> new RuntimeException("Teilplan mit teil_id = " + knoten + " nicht gefunden."));
                 calcTeil(innerNode);
+                specialCase = true;
             }
             // find arbeitsplan steps
             List<CsvColumArbeitsplan> arbeitsplan = arbeitsplanList.stream()
@@ -52,7 +59,7 @@ public class AblaufPlanCalculator {
                     .sorted(Comparator.comparingInt(CsvColumArbeitsplan::getAg_nr))
                     .collect(Collectors.toList());
             for (CsvColumArbeitsplan csvColumArbeitsplan : arbeitsplan) {
-                addArbeitToResult(csvColumArbeitsplan);
+                addArbeitToResult2(csvColumArbeitsplan);
             }
         }
     }
@@ -108,5 +115,93 @@ public class AblaufPlanCalculator {
 
     private LocalDateTime toLocalDateTime(Date date) {
         return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+    LocalDateTime lastTime = LocalDate.of(2025, 1, 6).atTime(8, 0);
+    boolean specialCase = false;
+
+    private void addArbeitToResult2(CsvColumArbeitsplan csvColumArbeitsplan) {
+        int time = csvColumArbeitsplan.getDauer();
+        int maschine = csvColumArbeitsplan.getMaschine();
+        LocalDateTime currentTime = null;
+        int teilId = csvColumArbeitsplan.getTeil_id();
+
+        boolean isFirstStep = csvColumArbeitsplan.getAg_nr() == 1;
+        // sub node was processed so last time must be checked
+        if (specialCase) {
+            specialCase = false;
+            isFirstStep = false;
+        }
+        if (isFirstStep) {
+            currentTime = LocalDate.of(2025, 1, 6).atTime(8, 0);
+        } else {
+            currentTime = lastTime;
+        }
+
+        // filter for maschine
+        List<CsvColumn> onlyGivenMaschine = result.stream()
+                .filter(a -> a.getMaschine() == maschine)
+                .sorted(Comparator.comparing(CsvColumn::getBeginnDerFertigung))
+                .collect(Collectors.toList());
+
+        // check special cases
+        if (onlyGivenMaschine.size() == 0) {
+            CsvColumn entry = new CsvColumn(
+                    maschine,
+                    toDate(currentTime),
+                    toDate(currentTime.plusMinutes(time)),
+                    teilId);
+            result.add(entry);
+            lastTime = currentTime.plusMinutes(time);
+            return;
+        }
+
+        CsvColumn firstEntry = onlyGivenMaschine.get(0);
+        LocalDateTime endTime = currentTime.plusMinutes(time);
+        // there is a timespot between t0 and the first entry
+        if (endTime.isBefore(toLocalDateTime(firstEntry.getBeginnDerFertigung()))) {
+            CsvColumn entry = new CsvColumn(
+                    maschine,
+                    toDate(currentTime),
+                    toDate(endTime),
+                    teilId);
+            result.add(entry);
+            lastTime = endTime;
+            return;
+        }
+        // else check for spots
+        CsvColumn entry = onlyGivenMaschine.stream()
+                .reduce((a, b) -> {
+                    var a_end = toLocalDateTime(a.getEndeDerFertigung());
+                    var b_begin = toLocalDateTime(b.getBeginnDerFertigung());
+                    var newEnd = a_end.plusMinutes(time);
+                    // check if time is after 16 pm
+                    if (newEnd.toLocalTime().isAfter(LocalTime.of(16, 0))) {
+                        // get next working day
+                        newEnd = naechsterArbeitstag(newEnd.toLocalDate());
+                        newEnd.plusMinutes(time);
+
+                        if (newEnd.isBefore(b_begin)) {
+                            return a; // time frame is big enough
+                        }
+                        return b; // time frame is to small
+                    }
+                    // check if process ends befor beginning of b
+                    if (newEnd.isBefore(b_begin)) {
+                        return a; // time frame is big enough
+                    } else {
+                        return b; // time frame is to small
+                    }
+                }).get();
+
+        var entry_end = toLocalDateTime(entry.getEndeDerFertigung());
+        var newEnd = entry_end.plusMinutes(time);
+        if (newEnd.toLocalTime().isAfter(LocalTime.of(16, 0))) {
+            entry_end = naechsterArbeitstag(newEnd.toLocalDate());
+            newEnd = entry_end.plusMinutes(time);
+        }
+        CsvColumn entry2 = new CsvColumn(maschine, toDate(entry_end), toDate(newEnd), teilId);
+        result.add(entry2);
+        lastTime = newEnd;
     }
 }
